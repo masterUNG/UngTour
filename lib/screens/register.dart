6ae5e9ef7569ba2ef05fbfb2ab215ff767124303
masterUNG <phrombutr@gmail.com>
2019-12-21 11:19:14 +0700

    update

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -6,12 +6,47 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  // Field
+
+  // Method
+
+  Widget cameraButton() {
+    return RaisedButton.icon(
+      icon: Icon(Icons.add_a_photo),
+      label: Text('Camera'),
+      onPressed: (){},
+    );
+  }
+
+  Widget showButton() {
+    return Row(
+      children: <Widget>[],
+    );
+  }
+
+  Widget showPicture() {
+    return Container(
+      margin: EdgeInsets.only(top: 30.0, bottom: 30.0),
+      height: MediaQuery.of(context).size.height * 0.4,
+      width: MediaQuery.of(context).size.width,
+      child: Image.asset(
+        'images/picture.png',
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Register'),
-      ),body: Text('body'),
+      ),
+      body: Column(
+        children: <Widget>[
+          showPicture(),
+        ],
+      ),
     );
   }
 }
